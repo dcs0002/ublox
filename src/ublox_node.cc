96581b30f379a783ff99126251fc4da73653c789
rcta <rcta@rcta0.qnao.net>
2012-10-12 01:01:22 -0400

    DR config

--- src/ublox_node.cc
+++ src/ublox_node.cc
@@ -90,7 +90,8 @@ int main (int argc, char** argv) {
   ublox_node.setup();
   ublox_node.ublox_.SetPortConfiguration(true,true,false,false);
   ublox_node.ublox_.ConfigureMessageRate(0x01,0x02,1);
-  // ublox_node.ublox_.ConfigureMessageRate(0x01,0x06,1);
+  ublox_node.ublox_.ConfigureMessageRate(0x01,0x06,1);
+  ublox_node.ublox_.ConfigureMessageRate(0x01,0x03,1);
 
 
   didpoll = ublox_node.ublox_.PollIniAid();
